c53eeab3022e482f2cb0cbf1a6867f1c456030e9
poie1967 <poie67@yandex.ru>
2025-03-03 16:36:17 +0300

    vesion_1

---
+++ game_v2.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0
+    low_num = 1
+    high_num = 100
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(low_num, high_num)  # предполагаемое число        
+        if number < predict_number:
+            high_num = predict_number
+        elif number > predict_number:
+            low_num = predict_number        
+        else:
+            number == predict_number
+            break  # выход из цикла, если угадали
+    return count
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = []  # список для сохранения количества попыток
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))  # находим среднее количество попыток
+
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
+    return score
+
+
+# RUN
+#if __name__ == '__main__':
+score_game(random_predict)
