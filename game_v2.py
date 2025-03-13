1e31361085e42dcb063eb709b05de26177c9478b
poie1967 <poie67@yandex.ru>
2025-03-13 08:52:36 +0300

    v3

--- game_v2.py
+++ game_v2.py
@@ -14,7 +14,7 @@ def random_predict(number: int = 1) -> int:
     count = 0  # устанавливаем счётчик попыток в 0
     # приравниваем текущие верхние и нижние границы к заданным для случайного числа
     low_num = low_border
-    high_num = high_border - 1
+    high_num = high_border
 
     while True:
         count += 1
@@ -33,7 +33,7 @@ def random_predict(number: int = 1) -> int:
     return count
 
 
-low_border, high_border = 1, 101  # Задаём границы для случайного числа
+low_border, high_border = 1, 1000001  # Задаём границы для случайного числа
 
 
 def score_game(random_predict) -> int:
@@ -49,15 +49,19 @@ def score_game(random_predict) -> int:
     count_ls = []  # список для сохранения количества попыток
     np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(
-        low_border, high_border, size=(100)
+        low_border, high_border, size=(10000)
     )  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))  # находим среднее количество попыток
+    score_min = int(np.min(count_ls))  # находим минимальное количество попыток
+    score_max = int(np.max(count_ls))  # находим минимальное количество попыток
 
     print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
+    print(f"Ваш алгоритм угадывает число в минимум за: {score_min} попыток")
+    print(f"Ваш алгоритм угадывает число в максимум за: {score_max} попыток")
     return score
 
 
